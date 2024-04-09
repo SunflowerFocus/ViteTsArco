c0140856dc07f3fbf3a15d3f9933e877793514b3
pan.feng <pan.feng@scrcnet.org>
2024-04-09 15:20:08 +0800

    some

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: { // 服务器选项
+    port: 3100, // 服务器主机
+    open: false, // 是否自动打开浏览器
+    strictPort: true // 端口占用退出
+  },
+
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, './'),
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   plugins: [vue()],
 })
